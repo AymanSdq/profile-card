97e41e3efbeaca3d78c72f0059b73df3108c641b
Aymansdq <sedqiayman02@gmail.com>
2024-07-26 22:25:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <section>
+        <h1 className='p-4 bg-fuchsia-400'>Hallo zusammen!</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
