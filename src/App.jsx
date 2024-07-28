02a8af83991757e9bdcc9796c5685b198820843b
AymanSdq <sedqiayman02@gmail.com>
2024-07-28 13:21:47 +0100

    Bg with circles

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,14 @@ function App() {
 
   return (
     <>
-      <section>
-        <h1 className='p-4 bg-fuchsia-400'>Hallo zusammen!</h1>
-      </section>
+      <main className='relative bg-darkCyan w-full h-screen flex justify-center items-center'>
+        <img className='absolute left-0 top-0' src="/bg-pattern-top.svg" alt="Top Circle" />
+        <img className='absolute right-0 bottom-0' src="/bg-pattern-bottom.svg" alt="Bottom Circle" />
+        {/* Container of the card */}
+        <section className=''>
+
+        </section>
+      </main>
     </>
   )
 }
