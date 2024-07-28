a35715402e201f481e4f4b386349a67c09794714
AymanSdq <sedqiayman02@gmail.com>
2024-07-28 12:57:12 +0100

    Fixing colors and text

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkCyan : "hsl(185, 75%, 39%)",
+        veryDarkBlue : "hsl(229, 23%, 23%)",
+        darkGrayish : "hsl(227, 10%, 46%)",
+        darkGray : "hsl(0, 0%, 59%)"
+      },
+    },
   },
   plugins: [],
 }
